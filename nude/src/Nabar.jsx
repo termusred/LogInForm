3a53fc64472cda53ad36d74848a15677fa942b91
Muslima <asimovamuslima17@gmail.com>
2024-04-30 19:32:52 +0500

    bruhgit add .!

---
+++ nude/src/Nabar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css"
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className="con1"></div>
+            <div className="con2"></div>
+            <div className="con3"></div>
+        </nav>
+    )
+}
